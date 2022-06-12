498b1fde6c726b04c731f546176d962e6a711e07
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-06-12 02:45:39 -0300

    update proyectos

--- src/components/proyectos/Proectos.jsx
+++ src/components/proyectos/Proectos.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import s from "./proyectos.module.css";
 import ReactPlayer from "react-player";
+import bookflix from "../../../icons/bookflix 1.png";
 
 export const Proectos = () => {
+  let urlBookflix = "https://booksflix.vercel.app/";
   let primerProyecto = "https://youtu.be/uBZSd2uPU9U";
   return (
-    <div>
+    <>
       <h2 className={s.h2}>Project</h2>
-      <div className={s.appPokemon}>
-        <h3 className={s.h3}>App pokemon</h3>
-        <ReactPlayer
-          url={primerProyecto}
-          controls={true}
-          muted
-          width="250px"
-          height="150px"
-        />
+      <div className={s.contenedorProyecto}>
+        <div className={s.appPokemon}>
+          <h3 className={s.h3}>App pokemon</h3>
+          <ReactPlayer
+            url={primerProyecto}
+            controls={true}
+            muted
+            width="250px"
+            height="150px"
+          />
+        </div>
+        <div className={s.bookflix}>
+          <h3 className={s.h3}>Bookflix</h3>
+          <a href={urlBookflix}>
+            <img src={bookflix} alt="bookflix" width="250PX" height="150px" />
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
